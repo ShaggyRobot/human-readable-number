234a39f979c1575a3397470534a610291b30373d
ShaggyRobot <shaggyrobot@gmail.com>
2021-02-25 15:37:25 +0300

    feat: Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
-module.exports = function toReadable (number) {
-  
-}
+teens = [
+    "",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+];
+
+adults = [
+    "",
+    "",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+];
+
+module.exports = function toReadable(number) {
+    hundreds = Math.floor(number / 100);
+    tens = number % 100;
+    digits = tens % 10;
+
+    console.log(tens);
+
+    forHuman = `${teens[hundreds] ? teens[hundreds] + " " + "hundred " : ""}${
+        tens < 20
+            ? teens[tens]
+            : adults[Math.floor(tens / 10)] + " " + teens[tens % 10]
+    }`;
+    return forHuman ? forHuman.trim() : "zero";
+};
